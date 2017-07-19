bad3faeff847a95dd1173fc6fb0669aef14eb097
github username <github email address>
2017-07-19 17:43:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+    	erb :'index.html'
+    end
 
-  end
+    get '/new' do
+    	erb :'pirates/new'
+    end
+
+    post '/pirates' do 
+		@pirate = Pirate.new(name: params['name'], weight: params['weight'], height: params['height'])
+			# can use create because the model inherits from ActiveRecord base 
+		# OR, could do:
+		# redirect to "/pirates" 
+		
+	end
+ 
+end
 end
